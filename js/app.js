f0e7ea0147cca4222bc4503194cf1e8cb1d9f6ab
DESKTOP-0I7PUQR\Jing <376952372@qq.com>
2018-01-05 17:05:24 +0800

    feature:initiation add some code to set up and initiate first push

---
+++ js/app.js
@@ -0,0 +1,57 @@
+// 洗牌函数来自于 http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+/*
+ * 创建一个包含所有卡片的数组
+ */
+/*
+ * 显示页面上的卡片
+ *   - 使用下面提供的 "shuffle" 方法对数组中的卡片进行洗牌
+ *   - 循环遍历每张卡片，创建其 HTML
+ *   - 将每张卡的 HTML 添加到页面
+ */
+jQuery(document).ready(function () {
+            var mycards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
+                    "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt",
+                    "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
+                    "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+            mycards = shuffle(mycards);
+            $(".fa").attr("class", "fa");
+            //将原有的类别清除
+            
+            var i;
+            for (i = 0; i < 16; i++) {
+                $($(".card .fa")[i]).addClass(mycards[i]);
+            }
+    
+    
+});
+
+
+
+
+
+
+
+/*
+ * 设置一张卡片的事件监听器。 如果该卡片被点击：
+ *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *  - 将卡片添加到状态为 “open” 的 *数组* 中（将这个功能放在你从这个函数中调用的另一个函数中）
+ *  - 如果数组中已有另一张卡，请检查两张卡片是否匹配
+ *    + 如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
+ */
