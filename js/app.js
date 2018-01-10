b2c1d0f4537f693c401456b854fc9f1ca1c65b88
USER-20161201OC\Administrator <376952372@qq.com>
2018-01-10 21:56:51 +0800

    fix:fix some errors in app.js some objects in fa class was reset in wrong way.

--- js/app.js
+++ js/app.js
@@ -16,6 +16,14 @@ function shuffle(array) {
 /*
  * 创建一个包含所有卡片的数组
  */
+var mycards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
+                    "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt",
+                    "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
+                    "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+var open = new Array();
+
+var moveC=0;
+var pairOfCards=0;
 /*
  * 显示页面上的卡片
  *   - 使用下面提供的 "shuffle" 方法对数组中的卡片进行洗牌
@@ -23,32 +31,74 @@ function shuffle(array) {
  *   - 将每张卡的 HTML 添加到页面
  */
 jQuery(document).ready(function () {
-            var mycards = ["fa-diamond", "fa-diamond", "fa-paper-plane-o", "fa-paper-plane-o",
-                    "fa-anchor", "fa-anchor", "fa-bolt", "fa-bolt",
-                    "fa-cube", "fa-cube", "fa-leaf", "fa-leaf",
-                    "fa-bicycle", "fa-bicycle", "fa-bomb", "fa-bomb"];
+            $(".moves").text(moveC);
+    
             mycards = shuffle(mycards);
-            $(".fa").attr("class", "fa");
+            $(".card .fa").attr("class", "fa");
             //将原有的类别清除
-            
             var i;
             for (i = 0; i < 16; i++) {
                 $($(".card .fa")[i]).addClass(mycards[i]);
             }
     
-    
 });
 
-var open = new Array();
+
 var openCard = function (cardID) {
     $($(".card")[cardID]).addClass("show open");
-    var CardName=$($(".card i")[cardID]).attr("class");
-    open.push(1);
+    /*var CardName=$($(".card i")[cardID]).attr("class");*/
+    open.push(cardID);
+}
+
+var matchCard=function(){
+    $($(".card")[open[0]]).removeClass("show open");
+    $($(".card")[open[0]]).addClass("match");
+    
+    $($(".card")[open[1]]).removeClass("show open");
+    $($(".card")[open[1]]).addClass("match");
+    
+    pairOfCards=pairOfCards+1;
+    open.length=0;
+
+}
+
+var resetCard=function(){
+    $($(".card")[open[0]]).removeClass("show open");
+    $($(".card")[open[1]]).removeClass("show open");
+    open.length=0;
+
+}
+
+var moveCounter=function(){
+    moveC=moveC+1;
+    $(".moves").text(moveC);
+
+}
+
+var gameOver=function(){
+
+
 }
 
 $(".card").click(function(event) {
     var cardID = $(".deck").find("li").index($(event.target));
     openCard(cardID);
+    var openNum=open.length;
+    
+    if(openNum==2){
+        /*如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）*/
+        if(mycards[open[0]]==mycards[open[1]]){
+            setTimeout(matchCard,500);
+            
+            /*如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）*/
+        }
+        else{
+            setTimeout(resetCard,500);
+            /*如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号*/  
+        }  
+    }
+    /*增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）*/
+    moveCounter();
 });
 
 
